76daa5340fe8beb7ae529975dc3b5c12646a0abe
KyleMcDonell <KyleMcDonell@gmail.com>
2016-04-29 19:02:21 -0400

    Added audio?

--- face_sound.py
+++ face_sound.py
@@ -1,5 +1,6 @@
 import cv2
 import sound
+import pyaudio
 import numpy as np
 
 #returns normalized location of face in image (x,y)
@@ -15,16 +16,13 @@ def get_sound(frame, faces):
     root = sound.Note('A',3)
     scale = sound.Scale(root,[2, 2, 1, 2, 2, 2, 1])
 
-    chunks = []
-    tones =np.empty((88200,1))
-    for face in faces:
-        tones+sound.pluck2(scale.get(map2sound(frame,face)))
-        #chunks.append(sound.pluck2(sound.scale.get(map2sound(frame,face))))
+    return np.sum([sound.pluck2(scale.get(map2sound(frame, face))) for face in faces])
 
 
 def main():
     faceCascade = cv2.CascadeClassifier("./faces/haarcascade_frontalface_alt.xml")
     video_capture = cv2.VideoCapture(0)
+
 
     while True:
         # Capture frame-by-frame
@@ -41,15 +39,26 @@ def main():
         # Draws a dot at the center of the faces
         for (x, y, w, h) in faces:
             cv2.circle(frame, (x+w/2,y+h/2), 2,(0, 255, 0),2 )
-        get_sound(frame, faces)
-        for face in faces:
-            print map2sound(frame,face)
-            # print get_face_loc(frame, face)   
+
+
+
+
         # Display the resulting frame
         cv2.imshow('Video', frame)
 
+        # Get the sound and play it
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=1)
+        sound = get_sound(frame, faces)
+        stream.write(sound.astype(np.float32).tostring())
+        stream.close()
+        p.terminate()
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+
+
 
     # When everything is done, release the capture
     video_capture.release()
